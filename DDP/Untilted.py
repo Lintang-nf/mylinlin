6ee600e903267e71f73b6a50c33853872d391b8a
Lintang-nf <lintangnf3@gmail.com>
2022-10-22 15:06:26 +0700

    Biodata

---
+++ DDP/Untilted.py
@@ -0,0 +1,23 @@
+#SOAL NO. 1
+NamaDepan = "Lintang Kasyfi"
+TempatLahir = "Jakarta"
+TanggalLahir = "22 Ocktober 2003"
+Alamat = '''Jl. Rawamaya 1 no. 83'''
+Umur = "19 Tahun"
+NoTelp = int(6281617561936)
+Tinggi = "185cm"
+Hobi = "Main game"
+
+print(NamaDepan, type(NamaDepan))
+print(TempatLahir, type(TempatLahir))
+print(TanggalLahir, type(TanggalLahir))
+print(Alamat, type(Alamat))
+print(Umur, type(Umur))
+print(NoTelp, type(NoTelp))
+print(Tinggi, type(Tinggi))
+print(Hobi, type(Hobi))
+
+#SOAL NO. 2
+print(19>17) #umur
+print(185<150) #tinggi
+print(22==20) #tanggal
