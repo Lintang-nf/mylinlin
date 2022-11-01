e255c79bf83e8f0658a97127ed637a788bb01908
Lintang-nf <lintangnf3@gmail.com>
2022-11-01 11:07:30 +0700

    Add Pratktikum-06

---
+++ DDP/Menghitung dan mengubah.py
@@ -0,0 +1,28 @@
+print ("Program Menghitung Keliling dan Jajargenjang")
+
+a =int(input("Masukkan Alasnya     ="))
+t =int(input("Masukkan Tingginya   ="))
+sa=int(input("Masukkan Sisi 1      ="))
+sb=int(input("Masukkan Sisi 2      ="))
+
+luas = a*t
+keliling = 2* (sa+sb)
+
+print("Luas Jajargenjang Adalah      =", luas)
+print("Keliling Jajargenjang Adalah  =", luas)
+
+#Mengubah suhu fahrenheit ke celcius dan reamur
+
+print ("Program Mengubah Suhu Fahrenheit Ke Reamur dan Celcius")
+x= int(input("Masukkan suhu dalam Fahrenheit :"))
+reamur  = x - 32 * 4/9
+celcius = x - 32 * 5/9
+print ("Suhu dalam Reamur Adalah  :", reamur)
+print ("Suhu dalam Celcius Adalah :", celcius)
+
+#menghitung berat badan ideal
+
+print("Program Menghitung Berat Badan Ideal")
+x - int(input("masukan Tinggi Badan :"))
+bbideal = x-110
+print ("Berat Badan yang Ideal Adalah :", bbideal)
